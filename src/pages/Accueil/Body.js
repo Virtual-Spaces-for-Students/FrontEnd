2a61200ee41533e3907be4746551e4f13d0867fe
Machurui <francois.serra00@gmail.com>
2022-09-19 16:27:23 +0200

    Front Close

--- src/pages/Accueil/Body.js
+++ src/pages/Accueil/Body.js
@@ -14,9 +14,8 @@ export default function BodyHome() {
                 </h1>
             </div>
             <div className="w-full flex flex-col items-center justify-center p-44 md:py-8 lg:py-12 xl:py-16 md:px-8 lg:px-12 xl:px-20">
-                <iframe width="1080" height="720" src="https://172.18.10.101:8006/:101/console=kvm&novne=18vmid=1008vmname=debian-test&node-vss-virt&resize=off&cmd= " frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                <iframe width="1080" height="720" src="https://www.youtube.com/embed/SWtT4X1sNlU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
             </div>
         </div>
     );
 }
-//https://www.youtube.com/embed/SWtT4X1sNlU
