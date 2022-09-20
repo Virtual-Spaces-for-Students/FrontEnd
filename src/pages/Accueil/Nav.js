308a45ac9dee5115a665ce92be1f41d002796c6a
Machurui <62929512+Machurui@users.noreply.github.com>
2022-09-20 15:14:11 +0200

    Fix : A balise

--- src/pages/Accueil/Nav.js
+++ src/pages/Accueil/Nav.js
@@ -12,9 +12,9 @@ export default function NavBarHome() {
                         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Virtual Spaces for Students</span>
                     </a>
                     <div className="flex md:order-2">
-                        <button type="button" href="https://app.vss.local:8080/auth/login" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-bold rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <a type="button" href="https://app.vss.local:8080/auth/login" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-bold rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             Se connecter
-                        </button>
+                        </a>
                     </div>
                     <div className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
                         <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
